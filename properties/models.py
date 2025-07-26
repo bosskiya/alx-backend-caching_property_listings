9df8af3742fddd0cf8674f9cbaf2aeb8e8693640
binikiya <biniyamkiya3682@gmail.com>
2025-07-26 19:58:02 +0300

    Alx Backend Caching Property Listing

--- properties/models.py
+++ properties/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Propery(models.Model):
+class Property(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
