60671faea3b0a2d674d0e4a214ae4f13aa570a97
binikiya <biniyamkiya3682@gmail.com>
2025-07-26 20:17:16 +0300

    Alx Backend Caching Property Listing

--- properties/views.py
+++ properties/views.py
@@ -4,5 +4,7 @@ from .models import Property
 
 @cache_page(60 * 15)
 def property_list(request):
-    if request.method != 'GET':
-        return JsonResponse({'error': 'Method not allowed'}, status=405)
+    properties = Property.objects.all().values(
+        'id', 'title', 'description', 'price', 'location', 'created_at'
+    )
+    return JsonResponse(list(properties), safe=False)
